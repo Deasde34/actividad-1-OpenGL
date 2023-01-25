133ad7c7310a1e8c14060eb59871fba5e1795fa2
Deasde34 <51971559+Deasde34@users.noreply.github.com>
2023-01-25 09:52:28 -0700

    Add files via upload

--- ventana_opengl.py
+++ ventana_opengl.py
@@ -1,6 +1,15 @@
 from OpenGL.GL import *
 import glfw
 
+def dibujar():
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0,0.0,0.0)
+    glVertex3f(-1.0,0.0,0.0)
+    glVertex3f(0.0,1.0,0.0)
+    glVertex3f(1.0,0.0,0.0)
+
+    glEnd()
+    
 def main():
     width = 400
     height = 600
@@ -25,7 +34,7 @@ def main():
         glClearColor(0.5, 0.5, 0.5, 1.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         #DIBUJAR
-        #ESTO QUEDA PENDIENTE
+        dibujar()
         glfw.poll_events()
         glfw.swap_buffers(window)
     glfw.destroy_window(window)
